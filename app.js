083556a118e2805e0b359207766ceb8012ddd3a7
masila <abelmasila@gmail.com>
2018-04-23 23:57:11 +0300

    Added yargs address input support

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
 const request= require('request');
 
+const yargs=require('yargs');
+
+const argv=yargs.
+    options({
+    a: {
+        demand:true,
+        alias:'address',
+        describe: 'Address to fetch weather for',
+        string:true
+    } 
+    })
+    .help()
+    .alias('help','h')
+    .argv;
+ console.log(argv.a);
+ const encodedAddress=encodeURIComponent(argv.address);
+
 request({
-    url: 'https://maps.googleapis.com/maps/api/geocode/json?address=chaka%20place',
+    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
     json: true
 }, (error, response, body)=>{
-    console.log("Printing body", JSON.stringify(body, undefined, 2));
+   console.log(`Address ${body.results[0].formatted_address}`);
+   console.log(`Lat ${body.results[0].geometry.location.lat}`)
+   console.log(`Long ${body.results[0].geometry.location.lng}`)
 });
