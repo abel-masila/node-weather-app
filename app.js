36414258b1516ac2127f98de55f6a71ff459ce3d
masila <abelmasila@gmail.com>
2018-04-24 00:08:57 +0300

    added error handling

--- app.js
+++ app.js
@@ -21,7 +21,14 @@ request({
     url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
     json: true
 }, (error, response, body)=>{
-   console.log(`Address ${body.results[0].formatted_address}`);
-   console.log(`Lat ${body.results[0].geometry.location.lat}`)
-   console.log(`Long ${body.results[0].geometry.location.lng}`)
+    if(error){
+        console.log('Unable to connect to google servers');
+    } else if(body.status==='ZERO_RESULTS'){
+        console.log('Unable to find that address');
+    } else if(body.status==='OK'){
+        console.log(`Address ${body.results[0].formatted_address}`);
+        console.log(`Lat ${body.results[0].geometry.location.lat}`);
+        console.log(`Long ${body.results[0].geometry.location.lng}`);
+    }
+   
 });
